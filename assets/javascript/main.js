5b04621b19a44c6603b0435059c198021e091253
MesayBeyene <mesibella@gmail.com>
2019-06-22 23:21:25 -0700

    second Commit

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,6 +3,6 @@
 <a href="mailto:example@yourdomain.com"><img src="picture_path.jpg" /></a>
 
 
-try <button onclick="window.open('mailto:KingRider<contato@sandroalvares.com.br>');">Contact me</button>
+
 
 window.location.href = "mailto:address@dmail.com";
