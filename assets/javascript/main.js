22b6eb7686af34829b438b8c5e1eed992bbfa442
MesayBeyene <mesibella@gmail.com>
2019-06-22 18:53:51 -0700

    First Commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,8 @@
+
+
+<a href="mailto:example@yourdomain.com"><img src="picture_path.jpg" /></a>
+
+
+try <button onclick="window.open('mailto:KingRider<contato@sandroalvares.com.br>');">Contact me</button>
+
+window.location.href = "mailto:address@dmail.com";
